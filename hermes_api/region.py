6465975a4eb2a4d9813e257328cf7164140a6bb5
Travis Beatty <travisby@gmail.com>
2013-11-02 16:20:27 -0400

    Added simple region abstraction

---
+++ hermes_api/region.py
@@ -0,0 +1,11 @@
+class Region(object):
+    code = ''
+
+NORTHAMERICA = Region()
+NORTHAMERICA.code = 'NA'
+
+EUROPE = Region()
+EUROPE.code = 'EU'
+
+ASIAPACIFIC = Region()
+ASIAPACIFIC.code = 'AP'
