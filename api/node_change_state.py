a3ea43724a3f3fbd10bc23a304029bacb3eaed14
Travis Beatty <travisby@gmail.com>
2013-11-01 22:11:30 -0400

    Added NodeChangeState model

---
+++ api/node_change_state.py
@@ -0,0 +1,18 @@
+import json
+
+
+class NodeChangeState(object):
+    node_count = 0
+    effective_after_turns = 0
+
+    def __init__(self, node_count=0, effective_after_turns=0):
+        self.node_count = node_count
+        self.effective_after_turns = effective_after_turns
+
+    def __repr__(self):
+        return json.dumps(
+            {
+                'NodeCount': self.node_count,
+                'EffectiveAfterTurns': self.effective_after_turns
+            }
+        )
