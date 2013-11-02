8e6da5c37705660945709b05783ffe0921f1bdf8
Travis Beatty <travisby@gmail.com>
2013-11-01 21:09:48 -0400

    Added Request Object

---
+++ api/request.py
@@ -0,0 +1,18 @@
+import json
+
+class Request:
+    command = ''
+    token = ''
+    change_request = None
+
+    def __init__(self, command='', token='', change_request=None):
+        self.command = command
+        self.token = token
+        self.change_request = change_request
+
+    def __repr__():
+        return json.dumps(
+            'Command': self.command,
+            'Token': self.token,
+            'ChangeRequest': self.change_request
+        )
